9b8987ace49f00ec06ac0dd82468a2aac0c4dbe1
jhreiser <jordanreiser1255@gmail.com>
2020-12-16 13:14:25 -0500

    Everything works in the simple task app.

---
+++ Tasks/TaskViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TaskViewController.swift
+//  Tasks
+//
+//  Created by Jordan Reiser on 12/16/20.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
